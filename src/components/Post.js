0edcc51adaae75b430e4e879ac52314b86df95b6
PeaceZay <zay.smith007@gmail.com>
2022-11-04 21:06:58 -0500

    Added details component

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,11 +1,11 @@
 // import BodyCard from './BodyCard';
-
+import Details from "./Details"
 const Post = ({ story }) => {
   return (
     // <BodyCard data={story} />
     <ul className='story-list'>
       <li className='story-itm' key={story.objectID}>
-        {story.title} (<a href={story.url}>{story.url}</a>)
+        {story.title} (<a href={story.url}>{story.url}</a>) <Details story ={story} />
       </li>
     </ul>
   );
